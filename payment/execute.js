fc690ad123c42d84ce7f6902a5f3b874f7d4a7a9
Walid Nasim <walidnasim@Walids-MacBook-Pro.local>
2018-02-05 10:36:31 -0500

    SDfdsf

--- payment/execute.js
+++ payment/execute.js
@@ -1,49 +1,55 @@
-{ intent: 'sale',
-  payer: { payment_method: 'paypal' },
-  redirect_urls:
-   { return_url: 'http://localhost:8000/success',
-     cancel_url: 'http://localhost:8000/cancel' },
-  transactions: [{
-    item_list: {
-       items: [{
-        name: 'Red Sox Hat',
-        sku: '001',
-        price: '25.00',
-        currency: 'USD',
-        quantity: 1
-      }]
-     },
-       amount: {
-         currency: 'USD',
-         total: '25'
-      },
-       description: 'Grub for the Hub'
-     }]
-  }
+// { intent: 'sale',
+//   payer: { payment_method: 'paypal' },
+//   redirect_urls:
+//    { return_url: 'http://localhost:8000/success',
+//      cancel_url: 'http://localhost:8000/cancel' },
+//   transactions: [{
+//     item_list: {
+//        items: [{
+//         name: 'Red Sox Hat',
+//         sku: '001',
+//         price: '25.00',
+//         currency: 'USD',
+//         quantity: 1
+//       }]
+//      },
+//        amount: {
+//          currency: 'USD',
+//          total: '25'
+//       },
+//        description: 'Grub for the Hub'
+//      }]
+//   }
+//
+//   {
+//     "intent": "sale",
+//     "payer": {
+//         "payment_method": "paypal"
+//     },
+//     "redirect_urls": {
+//         "return_url": "http://localhost:3000/success",
+//         "cancel_url": "http://localhost:3000/cancel"
+//     },
+//     "transactions": [{
+//         "item_list": {
+//             "items": [{
+//                 "name": "Red Sox Hat",
+//                 "sku": "001",
+//                 "price": "25.00",
+//                 "currency": "USD",
+//                 "quantity": 1
+//             }]
+//         },
+//         "amount": {
+//             "currency": "USD",
+//             "total": "25.00"
+//         },
+//         "description": "Hat for the best team ever"
+//     }]
+// };
 
-  {
-    "intent": "sale",
-    "payer": {
-        "payment_method": "paypal"
-    },
-    "redirect_urls": {
-        "return_url": "http://localhost:3000/success",
-        "cancel_url": "http://localhost:3000/cancel"
-    },
-    "transactions": [{
-        "item_list": {
-            "items": [{
-                "name": "Red Sox Hat",
-                "sku": "001",
-                "price": "25.00",
-                "currency": "USD",
-                "quantity": 1
-            }]
-        },
-        "amount": {
-            "currency": "USD",
-            "total": "25.00"
-        },
-        "description": "Hat for the best team ever"
-    }]
-};
+console.log('Before')
+setImmediate(()=>{
+  console.log('INside')
+})
+console.log('After')
